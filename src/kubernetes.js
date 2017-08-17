8d417e7dbec15488677ad341eb1859f9d91dc133
Josh Leder <slushie@gmail.com>
2017-08-16 23:06:42 -0600

    docs

--- src/kubernetes.js
+++ src/kubernetes.js
@@ -5,10 +5,22 @@ const Kube = require('kubernetes-client')
 const EventSource = require('./source')
 const debug = require('debug')('kubernetes-stream:api')
 
+/** @var config - cached kubeconfig */
+let config
+
 function getResourceVersion (object) {
   return _.get(object, 'metadata.resourceVersion')
 }
 
+/**
+ * A simple helper function to get a request object on
+ * the Kube client.
+ *
+ * @param kind
+ * @param apiVersion
+ * @param namespace
+ * @returns {ApiGroup}
+ */
 function createClient ({
   kind = 'pods',
   apiVersion = 'v1',
@@ -24,10 +36,16 @@ function createClient ({
   return client
 }
 
+/**
+ * Creates an EventSource for the Kubernetes client.
+ *
+ * @param client
+ * @returns {EventSource}
+ */
 function createEventSource (client = createClient()) {
   return new EventSource(
-    (options) => client.get({qs: options}), // list
-    (options, callback) => streamCallback( // watch
+    /* list  */ (options) => client.get({qs: options}),
+    /* watch */ (options, callback) => streamCallback(
       client.get({
         qs: Object.assign({
           watch: true
@@ -39,7 +57,13 @@ function createEventSource (client = createClient()) {
   )
 }
 
-let config
+/**
+ * Find a Kubeconfig from the environment. Looks for
+ * KUBECONFIG or KUBERNETES_SERVICE_HOST, or else
+ * falls back to reading from ~/.kube/config.
+ *
+ * @returns {config} suitable for passing to kubernetes-client
+ */
 function findKubernetesConfig () {
   if (config) {
     return config
@@ -65,13 +89,23 @@ function findKubernetesConfig () {
   return config
 }
 
+/**
+ * Take a Readable stream and pipe the chunks into a callback, or
+ * null at the end of the stream.
+ *
+ * Returns a "stop" function that will abort the stream.
+ *
+ * @param stream
+ * @param {callback} cb
+ * @returns {Function}
+ */
 function streamCallback (stream, cb) {
   stream.on('data', (chunk) => {
     cb(null, chunk)
   }).on('error', (err) => {
-    cb(err)
+    cb(err, null)
   }).on('end', () => {
-    cb()
+    cb(null, null)
   })
 
   return function () {
