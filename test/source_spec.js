6c8f76fa63108bcce0f178bcb3d1f0a3d749575c
Josh Leder <slushie@gmail.com>
2017-10-05 10:50:01 -0600

    spec EventSource

--- test/source_spec.js
+++ test/source_spec.js
@@ -42,7 +42,7 @@ describe('source.js', () => {
               .with.lengthOf(2)
           })
 
-          context('called with (null, apiList)', () => {
+          context('(null, apiList)', () => {
             it('emits a "list" event', (done) => {
               subject.list()
               const callback = listFnArgs[1]
@@ -57,10 +57,80 @@ describe('source.js', () => {
             })
           })
 
-          context('called with (err, null)', () => {
+          context('(err, null)', () => {
             it('emits an "error" event', (done) => {
               subject.list()
               const callback = listFnArgs[1]
+              const cbErr = new Error('test')
+
+              subject.on('error', (err) => {
+                expect(err).to.equal(cbErr)
+                done()
+              })
+
+              callback(cbErr)
+            })
+          })
+        })
+      })
+    })
+
+    describe('watchFn', () => {
+      let subject, watchFn, watchFnArgs
+      beforeEach('default watchFn', () => {
+        watchFnArgs = undefined
+        watchFn = (...args) => {
+          watchFnArgs = args
+        }
+      })
+
+      const RESOURCE_VERSION = 'test-rv'
+      const apiList = {
+        kind: 'PodList',
+        metadata: { resourceVersion: RESOURCE_VERSION },
+        items: []
+      }
+
+      beforeEach('create subject', () => {
+        subject = new EventSource(() => apiList, watchFn)
+      })
+
+      context('is called with', () => {
+        context('options', () => {
+          it('comes from .watch()', () => {
+            const localOpts = {option: 'value'}
+            subject.watch(localOpts)
+            expect(watchFnArgs[0]).to.equal(localOpts)
+          })
+        })
+
+        context('callback', () => {
+          it('is a function(err, apiEvent)', () => {
+            subject.watch()
+            const callback = watchFnArgs[1]
+            expect(callback).to.be.a('function')
+              .with.lengthOf(2)
+          })
+
+          context('(null, apiEvent)', () => {
+            it('emits a "event" event', (done) => {
+              subject.watch()
+              const callback = watchFnArgs[1]
+              const apiEvent = {type: 'Update', object: {kind: 'Pod'}}
+
+              subject.on('event', (event) => {
+                expect(event).to.equal(apiEvent)
+                done()
+              })
+
+              callback(null, apiEvent)
+            })
+          })
+
+          context('(err, null)', () => {
+            it('emits an "error" event', (done) => {
+              subject.watch()
+              const callback = watchFnArgs[1]
               const cbErr = new Error('test')
 
               subject.on('error', (err) => {
